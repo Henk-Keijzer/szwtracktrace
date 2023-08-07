147088e4248e44c41f1c98b57cd3e37e5de0ea92
Henk-Keijzer <96776484+Henk-Keijzer@users.noreply.github.com>
2023-08-07 17:09:12 +0200

    Version 3.0.4

--- lib/SZWconst.dart
+++ lib/SZWconst.dart
@@ -6,4 +6,4 @@ import 'package:flutter/material.dart';
 const server = 'https://tt.zeilvaartwarmond.nl/';
 const appIcon = 'whiteSZWicon';
 const boatIcon = Icons.sailing;  // Icons.rowing or Icons.directions_boat
-const maxReplay = 24;    // 0 = start of replay is at start of event, > 0 = start of replay is maxReplay hours ago
+const maxReplay = 0;    // 0 = start of replay is at start of event, > 0 = start of replay is maxReplay hours ago
