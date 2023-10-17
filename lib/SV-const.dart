be1ede0adfd636429d2be6e79c5e3bbd435fde60
Henk-Keijzer <96776484+Henk-Keijzer@users.noreply.github.com>
2023-10-17 16:25:09 +0200

    Version 3.1.3 Features - Floatingbutton voor auto volgen toegevoegd - Inhoud infowindow schepen aangepast - In live update per seconde met voorspelde positie op basis van laatst ontvangen info Bugfixes

--- lib/SV-const.dart
+++ lib/SV-const.dart
@@ -1,7 +1,5 @@
 //
 // constant with the URL of the our server
 //
-import 'package:flutter/material.dart';
-
 const server = 'https://sv.zeilvaartwarmond.nl/';
 const mobileAppAvailable = false;
--- lib/SZW-const.dart
+++ lib/SZW-const.dart
@@ -1,7 +1,5 @@
 //
 // constant with the URL of the our server
 //
-import 'package:flutter/material.dart';
-
 const server = 'https://tt.zeilvaartwarmond.nl/';
 const mobileAppAvailable = true;
